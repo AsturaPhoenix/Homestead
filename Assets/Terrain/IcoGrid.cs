f85b090ed1f0cfcd8e39b141d0b7214fbd37869e
Ross Wang <imagipioneer@gmail.com>
2020-08-08 17:02:42 -0700

    Add normals to IcoGrid

--- Assets/Terrain/IcoGrid.cs
+++ Assets/Terrain/IcoGrid.cs
@@ -1,41 +1,132 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 
-public class IcoGrid<T> {
-  public class Edge {
-    private readonly T[] vertices;
-    public readonly int a, b;
+/// <summary>
+/// Represents an icosahedral spherical lattice starting from Vector3.up, descending towards Vector3.forward, circling left-handed up.
+/// </summary>
+public class IcoGrid<T>
+{
+  // There are a bunch of ways to mesh the normals; let's do normalized linear interpolation for simplicity.
+  // Since we're interpolating linearly, each latitude is truly flat and increments linearly, so only the tropic Y is of consequence.
+  private static readonly float tropicY = 1 / Mathf.Sqrt(5);
+  // The rest of the normals will be (x, z), and we'll omit the poles since they'll be origin.
+  private static readonly Vector2[] icoNormals = new Vector2[10];
+  static IcoGrid() {
+    float sqrt5 = 1 / tropicY;
+    float tropicR = 2 * tropicY;
+    var lonVec = new Vector2(Mathf.Sqrt((5 + sqrt5) / 8), (sqrt5 - 1) / 4);
 
-    public Edge(IcoGrid<T> grid, int a, int b) {
-      vertices = grid.vertices;
-      this.a = a;
-      this.b = b;
-    }
+    icoNormals[0] = new Vector2(0, tropicR);
+    icoNormals[1] = tropicR * lonVec;
+    icoNormals[2] = new Vector2(
+      icoNormals[1].x * lonVec.y + icoNormals[1].y * lonVec.x,
+      icoNormals[1].y * lonVec.y - icoNormals[1].x * lonVec.x);
+    icoNormals[3] = new Vector2(-icoNormals[2].x, icoNormals[2].y);
+    icoNormals[4] = new Vector2(-icoNormals[1].x, icoNormals[1].y);
 
-    public T A {
-      get => vertices[a];
-      set => vertices[a] = value;
-    }
-    public T B {
-      get => vertices[b];
-      set => vertices[b] = value;
+    for (int i = 5; i < 10; ++i) {
+      icoNormals[i] = -icoNormals[(i - 3) % 5];
     }
   }
 
+  /// <summary>
+  /// Calculates the number of vertices in the equilateral pentagonal pyramid of the given height.
+  /// </summary>
+  private static int PyramidSize(int height) => 1 + 5 * height * (height - 1) / 2;
+
   public readonly int subdivisions;
+  public readonly T[] data;
 
-  public readonly T[] vertices;
+  private readonly int northTropic, southTropic;
 
-  public IcoGrid(int subdivisions) {
+  public IcoGrid(int subdivisions)
+    {
     if (subdivisions < 1) {
       throw new ArgumentOutOfRangeException("subdivisions", subdivisions, "subdivisions must be >= 1");
     }
 
     this.subdivisions = subdivisions;
-    vertices = new T[2 * subdivisions * 5 * subdivisions + 2];
+    data = new T[10 * subdivisions * subdivisions + 2];
+
+    northTropic = PyramidSize(subdivisions);
+    southTropic = northTropic + 5 * subdivisions * subdivisions;
   }
 
-  public IEnumerable<Edge> Edges {
+  private IEnumerable<Vector3> Normals {
+    get {
+      float inc = 1.0f / subdivisions;
+
+      // north pole
+      {
+        yield return Vector3.up;
+
+        for (int lat = 1; lat <= subdivisions; ++lat) {
+          float scale = lat * inc;
+          float y = Mathf.Lerp(1, tropicY, scale);
+
+          for (int face = 0; face < 5; ++face) {
+            Vector2 a = scale * icoNormals[face], b = scale * icoNormals[(face + 1) % 5];
+
+            for (int div = 0; div < lat; ++div) {
+              var v = Vector2.Lerp(a, b, (float)div / lat);
+              yield return new Vector3(v.x, y, v.y).normalized;
+            }
+          }
+        }
+      }
+
+      // tropics
+      {
+        for (int lat = 1; lat < subdivisions; ++lat) {
+          float t = lat * inc;
+          float y = Mathf.Lerp(tropicY, -tropicY, t);
+
+          for (int facePair = 0; facePair < 5; ++facePair) {
+            int next = (facePair + 1 % 5);
+            Vector2 qa = icoNormals[facePair], qb = icoNormals[next], qc = icoNormals[facePair + 5], qd = icoNormals[next + 5];
+            {
+              Vector2 a = Vector2.Lerp(qa, qc, t), b = Vector2.Lerp(qb, qc, t);
+              int len = subdivisions - lat;
+              for (int div = 0; div < len; ++div) {
+                var v = Vector2.Lerp(a, b, (float)div / len);
+                yield return new Vector3(v.x, y, v.y);
+              }
+            }
+            {
+              Vector2 a = Vector2.Lerp(qb, qc, t), b = Vector2.Lerp(qb, qd, t);
+              for (int div = 0; div < lat; ++div) {
+                var v = Vector2.Lerp(a, b, (float)div / lat);
+                yield return new Vector3(v.x, y, v.y);
+              }
+            }
+          }
+        }
+      }
+
+      // south pole
+      {
+        for (int lat = 0; lat < subdivisions; ++lat) {
+          int len = subdivisions - lat;
+          float scale = len * inc;
+          float y = Mathf.Lerp(-1, -tropicY, scale);
+
+          for (int face = 0; face < 5; ++face) {
+            Vector2 a = scale * icoNormals[face + 5], b = scale * icoNormals[(face + 1) % 5 + 5];
+
+            for (int div = 0; div < len; ++div) {
+              var v = Vector2.Lerp(a, b, (float)div / len);
+              yield return new Vector3(v.x, y, v.y).normalized;
+            }
+          }
+        }
+
+        yield return Vector3.down;
+      }
+    }
+  }
+
+  public IEnumerable<(int a, int b)> Edges {
     get {
       // We'll mostly be doing south-facing triangles.
 
@@ -44,50 +135,48 @@ public class IcoGrid<T> {
         int vertex(int lat, int face, int div) {
           if (lat == 0) return 0;
           // latLength = lat
-          return 1 + 5 * lat * (lat - 1) / 2 + (face * lat + div) % (5 * lat);
+          return PyramidSize(lat) + (face * lat + div) % (5 * lat);
         }
 
         for (int lat = 0; lat < subdivisions; ++lat) {
           for (int face = 0; face < 5; ++face) {
-            yield return new Edge(this, vertex(lat, face, 0), vertex(lat + 1, face, 0));
+            yield return (vertex(lat, face, 0), vertex(lat + 1, face, 0));
 
             for (int div = 0; div < lat; ++div) {
               int a = vertex(lat, face, div),
                   b = vertex(lat, face, div + 1),
                   c = vertex(lat + 1, face, div + 1);
-              yield return new Edge(this, a, b);
-              yield return new Edge(this, a, c);
-              yield return new Edge(this, b, c);
+              yield return (a, b);
+              yield return (a, c);
+              yield return (b, c);
             }
           }
         }
       }
 
       // tropics
-      int tropicsStart = 1 + 5 * subdivisions * (subdivisions - 1) / 2;
       {
         int latLength = 5 * subdivisions;
         // restart at lat = 0 for simplicity; faces omitted since each face and latitude is functionally identical until the next tropic
-        int vertex(int lat, int div) => tropicsStart + lat * latLength + div % latLength;
+        int vertex(int lat, int div) => northTropic + lat * latLength + div % latLength;
 
         for (int lat = 0; lat < subdivisions; ++lat) {
           for (int div = 0; div < latLength; ++div) {
             int a = vertex(lat, div),
                 b = vertex(lat, div + 1),
                 c = vertex(lat + 1, div);
-            yield return new Edge(this, a, b);
-            yield return new Edge(this, a, c);
-            yield return new Edge(this, b, c);
+            yield return (a, b);
+            yield return (a, c);
+            yield return (b, c);
           }
         }
       }
 
       // south pole
       {
-        int start = tropicsStart + 5 * subdivisions * subdivisions;
         int vertex(int lat, int face, int div) {
           int latLength = subdivisions - lat;
-          int latStart = start + 5 * lat * (subdivisions + latLength + 1) / 2;
+          int latStart = southTropic + 5 * lat * (subdivisions + latLength + 1) / 2;
           return lat == subdivisions ? latStart : latStart + (face * latLength + div) % (5 * latLength);
         }
 
@@ -97,10 +186,10 @@ public class IcoGrid<T> {
               int a = vertex(lat, face, div),
                   b = vertex(lat, face, div + 1),
                   c = vertex(lat + 1, face, div);
-              yield return new Edge(this, a, b);
+              yield return (a, b);
               if (div > 0)
-                yield return new Edge(this, a, c);
-              yield return new Edge(this, b, c);
+                yield return (a, c);
+              yield return (b, c);
             }
           }
         }
