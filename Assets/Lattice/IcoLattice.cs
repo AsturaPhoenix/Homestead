c0c6c7ebb201856fd0464cbd402a937672b92b09
Ross Wang <imagipioneer@gmail.com>
2022-07-01 20:13:55 -0700

    Adding pseudo-polar intermediate representation
    The vertex and edge calculations are already done in terms of this representation, so factoring it out makes those a
    little more natural and will allow us to reuse some of the math to project cartesian coordinates to faces. It does
    come at the cost of some repeated calculation for clarity.

--- Assets/Lattice/IcoLattice.cs
+++ Assets/Lattice/IcoLattice.cs
@@ -9,6 +9,107 @@ namespace Assets.Lattice
   /// </summary>
   public class IcoLattice : ILatticeGeometry<Vector3>
   {
+    public struct SubdividedCoordinate
+    {
+      public int face, div;
+
+      public SubdividedCoordinate(int face, int div) {
+        this.face = face;
+        this.div = div;
+      }
+      public override string ToString() {
+        return String.Format("({0}, {1})", face, div);
+      }
+
+
+      /// <summary>
+      /// Normalizes a subdivided coordinate to the range ([0, faces), [0, subdivisions)). It is worth noting that this handles negative signs differently
+      /// from naive integer arithmetic.
+      /// </summary>
+      public void Normalize(int faces, int subdivisions) {
+        face += div / subdivisions;
+        div %= subdivisions;
+
+        if (div < 0) {
+          --face;
+          div += subdivisions;
+        }
+
+        face %= faces;
+        if (face < 0) {
+          face += faces;
+        }
+      }
+    }
+
+    public struct PolarCoordinate
+    {
+      /// <summary>
+      /// lat down, lon left
+      /// 
+      /// In the tropics (lat face 1), longitudinal faces represent triangle pairs where the left triangle points down and the right points up.
+      /// 
+      /// Normalized form is up/left-inclusive, where the south pole is at latitude 3.0
+      /// </summary>
+      public SubdividedCoordinate lat, lon;
+
+      public PolarCoordinate(int latFace, int latDiv, int lonFace, int lonDiv) {
+        lat = new SubdividedCoordinate(latFace, latDiv);
+        lon = new SubdividedCoordinate(lonFace, lonDiv);
+      }
+
+      public override string ToString() {
+        return String.Format("({0}, {1})", lat, lon);
+      }
+
+      public void Normalize(int subdivisions) {
+        lat.Normalize(6, subdivisions);
+
+        if (lat.div == 0 && (lat.face == 0 || lat.face == 3)) { // poles
+          lon.face = lon.div = 0;
+        } else {
+          if (lat.face >= 3) {
+            lat.face = 5 - lat.face;
+            lat.div = subdivisions - lat.div;
+            Debug.Assert(lat.face >= 0 && lat.face < 3);
+
+            // To wrap latitude to the opposite side of the sphere, we need to flip things one way or another. One useful
+            // interpretation of this is to flip the face alignment as well, so that for any polar coordinate, its antipode is
+            // at lat.face + 3.
+            switch (lat.face) {
+              case 0:
+                lon.face -= 2;
+                break;
+              case 1:
+                lon.face -= 2;
+                lon.div -= lat.div;
+                break;
+              case 2:
+                lon.face += 2;
+                break;
+            }
+          }
+
+          lon.Normalize(5, lat.face == 0 ? lat.div : lat.face == 1 ? subdivisions : subdivisions - lat.div);
+        }
+      }
+
+      public static PolarCoordinate operator +(PolarCoordinate c, (int latDiv, int lonDiv) delta) {
+        c.lat.div += delta.latDiv;
+        c.lon.div += delta.lonDiv;
+        return c;
+      }
+
+      /// <summary>
+      /// Flips this polar coordinate to its antipode. The result is not normalized; normalization requires a subdivision
+      /// parameter.
+      /// </summary>
+      public static PolarCoordinate operator -(PolarCoordinate c) {
+        c.lat.face += 3;
+        return c;
+      }
+    }
+
     // There are a bunch of ways to mesh the normals; let's do normalized linear interpolation for simplicity.
     // Since we're interpolating linearly, each latitude is truly flat and increments linearly, so only the tropic Y is of consequence.
     private static readonly float tropicY = 1 / Mathf.Sqrt(5);
@@ -42,107 +143,143 @@ namespace Assets.Lattice
       this.subdivisions = subdivisions;
     }
 
+    public Vector3 ToCartesian(PolarCoordinate c) {
+      c.Normalize(subdivisions);
+
+      // Some of these calculations could be factored out and memoized for use cases such as vertex iteration, but that makes the code less reusable for use cases like projection.
+      switch (c.lat.face) {
+        case 0: { // north pole
+            if (c.lat.div == 0) {
+              return Vector3.up;
+            }
+
+            Vector2 a = icoNormals[c.lon.face], b = icoNormals[(c.lon.face + 1) % 5];
+            Vector2 v = Vector2.Lerp(a, b, (float)c.lon.div / c.lat.div);
+            float scale = (float)c.lat.div / subdivisions;
+            return (Vector3.up + new Vector3(v.x, tropicY - 1, v.y) * scale).normalized;
+          }
+        case 1: { // tropics
+            Vector2 t;
+            t.y = (float)c.lat.div / subdivisions;
+            int next = (c.lon.face + 1) % 5;
+            Vector2 qa = icoNormals[c.lon.face], qb = icoNormals[next], qc = icoNormals[c.lon.face + 5], qd = icoNormals[next + 5];
+
+            Vector2 a, b;
+            int boundary = subdivisions - c.lat.div;
+            if (c.lon.div < boundary) {
+              a = Vector2.Lerp(qa, qc, t.y);
+              b = Vector2.Lerp(qb, qc, t.y);
+              t.x = (float)c.lon.div / boundary;
+            } else {
+              a = Vector2.Lerp(qb, qc, t.y);
+              b = Vector2.Lerp(qb, qd, t.y);
+              t.x = (float)(c.lon.div - boundary) / c.lat.div;
+            }
+
+            var v = Vector2.Lerp(a, b, t.x);
+            return new Vector3(v.x, Mathf.Lerp(tropicY, -tropicY, t.y), v.y).normalized;
+          }
+        case 2: { //south pole
+            Vector2 a = icoNormals[c.lon.face + 5], b = icoNormals[(c.lon.face + 1) % 5 + 5];
+            int len = subdivisions - c.lat.div;
+            Vector2 v = Vector2.Lerp(a, b, (float)c.lon.div / len);
+            float scale = (float)len / subdivisions;
+            return (Vector3.down + new Vector3(v.x, 1 - tropicY, v.y) * scale).normalized;
+          }
+        default:
+          return Vector3.down;
+      }
+    }
+
+    public IEnumerable<PolarCoordinate> PolarCoordinates {
+      get {
+        {
+          PolarCoordinate c = new PolarCoordinate();
+          // north pole
+          yield return c;
+
+          for (c.lat.div = 1; c.lat.div < subdivisions; ++c.lat.div) {
+            for (c.lon.face = 0; c.lon.face < 5; ++c.lon.face) {
+              for (c.lon.div = 0; c.lon.div < c.lat.div; ++c.lon.div) {
+                yield return c;
+              }
+            }
+          }
+
+          //tropics
+          c.lat.face = 1;
+          for (c.lat.div = 0; c.lat.div < subdivisions; ++c.lat.div) {
+            for (c.lon.face = 0; c.lon.face < 5; ++c.lon.face) {
+              for (c.lon.div = 0; c.lon.div < subdivisions; ++c.lon.div) {
+                yield return c;
+              }
+            }
+          }
+
+          // south pole
+          c.lat.face = 2;
+          for (c.lat.div = 0; c.lat.div < subdivisions; ++c.lat.div) {
+            for (c.lon.face = 0; c.lon.face < 5; ++c.lon.face) {
+              int len = subdivisions - c.lat.div;
+              for (c.lon.div = 0; c.lon.div < len; ++c.lon.div) {
+                yield return c;
+              }
+            }
+          }
+
+          yield return new PolarCoordinate(3, 0, 0, 0);
+        }
+      }
+    }
+
+    /// <summary>
+    /// Calculates the number of vertices in the equilateral pentagonal pyramid of the given height.
+    /// </summary>
+    private static int PyramidSize(int height) => 1 + 5 * height * (height - 1) / 2;
+
+    public int ToIndex(PolarCoordinate c) {
+      c.Normalize(subdivisions);
+
+      // There's a lot that can be memoized here if we need to.
+
+      switch (c.lat.face) {
+        case 0:
+          return c.lat.div == 0? 0 : PyramidSize(c.lat.div) + c.lon.face * c.lat.div + c.lon.div;
+        case 1:
+          return PyramidSize(subdivisions) + c.lat.div * 5 * subdivisions + c.lon.face * subdivisions + c.lon.div;
+        case 2:
+          int latLen = subdivisions - c.lat.div;
+          return 2 * PyramidSize(subdivisions) + 6 * subdivisions * subdivisions - PyramidSize(latLen + 1) + c.lon.face * latLen + c.lon.div;
+        default:
+          return 2 * PyramidSize(subdivisions) + 6 * subdivisions * subdivisions - 1;
+      }
+    }
+
     public IEnumerable<Vector3> Vertices {
       get {
-        float inc = 1.0f / subdivisions;
-
-        // north pole
-        {
-          yield return Vector3.up;
-
-          for (int lat = 1; lat <= subdivisions; ++lat) {
-            float scale = lat * inc;
-            float y = Mathf.Lerp(1, tropicY, scale);
-
-            for (int face = 0; face < 5; ++face) {
-              Vector2 a = scale * icoNormals[face], b = scale * icoNormals[(face + 1) % 5];
-
-              for (int div = 0; div < lat; ++div) {
-                var v = Vector2.Lerp(a, b, (float)div / lat);
-                yield return new Vector3(v.x, y, v.y).normalized;
-              }
-            }
-          }
-        }
-
-        // tropics
-        {
-          for (int lat = 1; lat < subdivisions; ++lat) {
-            float t = lat * inc;
-            float y = Mathf.Lerp(tropicY, -tropicY, t);
-
-            for (int facePair = 0; facePair < 5; ++facePair) {
-              int next = (facePair + 1) % 5;
-              Vector2 qa = icoNormals[facePair], qb = icoNormals[next], qc = icoNormals[facePair + 5], qd = icoNormals[next + 5];
-              {
-                Vector2 a = Vector2.Lerp(qa, qc, t), b = Vector2.Lerp(qb, qc, t);
-                int len = subdivisions - lat;
-                for (int div = 0; div < len; ++div) {
-                  var v = Vector2.Lerp(a, b, (float)div / len);
-                  yield return new Vector3(v.x, y, v.y).normalized;
-                }
-              }
-              {
-                Vector2 a = Vector2.Lerp(qb, qc, t), b = Vector2.Lerp(qb, qd, t);
-                for (int div = 0; div < lat; ++div) {
-                  var v = Vector2.Lerp(a, b, (float)div / lat);
-                  yield return new Vector3(v.x, y, v.y).normalized;
-                }
-              }
-            }
-          }
-        }
-
-        // south pole
-        {
-          for (int lat = 0; lat < subdivisions; ++lat) {
-            int len = subdivisions - lat;
-            float scale = len * inc;
-            float y = Mathf.Lerp(-1, -tropicY, scale);
-
-            for (int face = 0; face < 5; ++face) {
-              Vector2 a = scale * icoNormals[face + 5], b = scale * icoNormals[(face + 1) % 5 + 5];
-
-              for (int div = 0; div < len; ++div) {
-                var v = Vector2.Lerp(a, b, (float)div / len);
-                yield return new Vector3(v.x, y, v.y).normalized;
-              }
-            }
-          }
-
-          yield return Vector3.down;
+        foreach (var polar in PolarCoordinates) {
+          yield return ToCartesian(polar);
         }
       }
     }
 
     public IEnumerable<(int a, int b)> Edges {
       get {
-        /// <summary>
-        /// Calculates the number of vertices in the equilateral pentagonal pyramid of the given height.
-        /// </summary>
-        int PyramidSize(int height) => 1 + 5 * height * (height - 1) / 2;
-
-        int northTropic = PyramidSize(subdivisions);
-        int southTropic = northTropic + 5 * subdivisions * subdivisions;
-
         // We'll mostly be doing south-facing triangles.
+
+        PolarCoordinate p = new PolarCoordinate();
 
         // north pole
         {
-          int vertex(int lat, int face, int div) {
-            if (lat == 0) return 0;
-            // latLength = lat
-            return PyramidSize(lat) + (face * lat + div) % (5 * lat);
-          }
+          for (p.lat.div = 0; p.lat.div < subdivisions; ++p.lat.div) {
+            for (p.lon.face = 0; p.lon.face < 5; ++p.lon.face) {
+              p.lon.div = 0;
+              yield return (ToIndex(p), ToIndex(p + (1, 0)));
 
-          for (int lat = 0; lat < subdivisions; ++lat) {
-            for (int face = 0; face < 5; ++face) {
-              yield return (vertex(lat, face, 0), vertex(lat + 1, face, 0));
-
-              for (int div = 0; div < lat; ++div) {
-                int a = vertex(lat, face, div),
-                    b = vertex(lat, face, div + 1),
-                    c = vertex(lat + 1, face, div + 1);
+              for (; p.lon.div < p.lat.div; ++p.lon.div) {
+                int a = ToIndex(p),
+                    b = ToIndex(p + (0, 1)),
+                    c = ToIndex(p + (1, 1));
                 yield return (a, b);
                 yield return (a, c);
                 yield return (b, c);
@@ -153,38 +290,34 @@ namespace Assets.Lattice
 
         // tropics
         {
-          int latLength = 5 * subdivisions;
-          // restart at lat = 0 for simplicity; faces omitted since each face and latitude is functionally identical until the next tropic
-          int vertex(int lat, int div) => northTropic + lat * latLength + div % latLength;
+          p.lat.face = 1;
 
-          for (int lat = 0; lat < subdivisions; ++lat) {
-            for (int div = 0; div < latLength; ++div) {
-              int a = vertex(lat, div),
-                  b = vertex(lat, div + 1),
-                  c = vertex(lat + 1, div);
-              yield return (a, b);
-              yield return (a, c);
-              yield return (b, c);
+          for (p.lat.div = 0; p.lat.div < subdivisions; ++p.lat.div) {
+            for (p.lon.face = 0; p.lon.face < 5; ++p.lon.face) {
+              for (p.lon.div = 0; p.lon.div < subdivisions; ++p.lon.div) {
+                int a = ToIndex(p),
+                    b = ToIndex(p + (0, 1)),
+                    c = ToIndex(p + (1, 0));
+                yield return (a, b);
+                yield return (a, c);
+                yield return (b, c);
+              }
             }
           }
         }
 
         // south pole
         {
-          int vertex(int lat, int face, int div) {
-            int latLength = subdivisions - lat;
-            int latStart = southTropic + 5 * lat * (subdivisions + latLength + 1) / 2;
-            return lat == subdivisions ? latStart : latStart + (face * latLength + div) % (5 * latLength);
-          }
+          p.lat.face = 2;
 
-          for (int lat = 0; lat < subdivisions; ++lat) {
-            for (int face = 0; face < 5; ++face) {
-              for (int div = 0; div < subdivisions - lat; ++div) {
-                int a = vertex(lat, face, div),
-                    b = vertex(lat, face, div + 1),
-                    c = vertex(lat + 1, face, div);
+          for (p.lat.div = 0; p.lat.div < subdivisions; ++p.lat.div) {
+            for (p.lon.face = 0; p.lon.face < 5; ++p.lon.face) {
+              for (p.lon.div = 0; p.lon.div < subdivisions - p.lat.div; ++p.lon.div) {
+                int a = ToIndex(p),
+                    b = ToIndex(p + (0, 1)),
+                    c = ToIndex(p + (1, 0));
                 yield return (a, b);
-                if (div > 0)
+                if (p.lon.div > 0)
                   yield return (a, c);
                 yield return (b, c);
               }
---
+++ Assets/Tests/IcoLatticeTest.cs
@@ -0,0 +1,143 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using Assets.Lattice;
+using UnityEngine;
+
+namespace Tests {
+  public class IcoLatticeTest {
+    public class PolarCoordinateTest
+    {
+      [Test]
+      public void NormalizeNorthPole() {
+        var c = new IcoLattice.PolarCoordinate(0, 0, 1, 1);
+        c.Normalize(3);
+        Assert.AreEqual(new IcoLattice.PolarCoordinate(), c);
+      }
+
+      [Test]
+      public void NormalizeNorthPoleAlias() {
+        var c = new IcoLattice.PolarCoordinate(5, 3, 1, 1);
+        c.Normalize(3);
+        Assert.AreEqual(new IcoLattice.PolarCoordinate(), c);
+      }
+
+      [Test]
+      public void NormalizeSouthPole() {
+        var c = new IcoLattice.PolarCoordinate(3, 0, 1, 1);
+        c.Normalize(3);
+        Assert.AreEqual(new IcoLattice.PolarCoordinate(3, 0, 0, 0), c);
+      }
+
+      [Test]
+      public void NormalizeSouthPoleAlias() {
+        var c = new IcoLattice.PolarCoordinate(2, 3, 1, 1);
+        c.Normalize(3);
+        Assert.AreEqual(new IcoLattice.PolarCoordinate(3, 0, 0, 0), c);
+      }
+
+      [Test]
+      public void NormalizeTropicNNE() {
+        var c = new IcoLattice.PolarCoordinate(1, -1, 0, -1);
+        c.Normalize(3);
+        Assert.AreEqual(new IcoLattice.PolarCoordinate(0, 2, 4, 1), c);
+      }
+
+      [Test]
+      public void NormalizeTropicNNW() {
+        var c = new IcoLattice.PolarCoordinate(1, -1, 4, 2);
+        c.Normalize(3);
+        Assert.AreEqual(new IcoLattice.PolarCoordinate(0, 2, 0, 0), c);
+      }
+
+      [Test]
+      public void NormalizeTropicNSE() {
+        var c = new IcoLattice.PolarCoordinate(1, 1, 0, -1);
+        c.Normalize(3);
+        Assert.AreEqual(new IcoLattice.PolarCoordinate(1, 1, 4, 2), c);
+      }
+
+      [Test]
+      public void NormalizeTropicNSW() {
+        var c = new IcoLattice.PolarCoordinate(1, 1, 4, 3);
+        c.Normalize(3);
+        Assert.AreEqual(new IcoLattice.PolarCoordinate(1, 1, 0, 0), c);
+      }
+
+      [Test]
+      public void NormalizeTropicSNE() {
+        var c = new IcoLattice.PolarCoordinate(1, 2, 0, -1);
+        c.Normalize(3);
+        Assert.AreEqual(new IcoLattice.PolarCoordinate(1, 2, 4, 2), c);
+      }
+
+      [Test]
+      public void NormalizeTropicSNW() {
+        var c = new IcoLattice.PolarCoordinate(1, 2, 4, 3);
+        c.Normalize(3);
+        Assert.AreEqual(new IcoLattice.PolarCoordinate(1, 2, 0, 0), c);
+      }
+
+      [Test]
+      public void NormalizeTropicSSE() {
+        var c = new IcoLattice.PolarCoordinate(1, 3, 0, -1);
+        c.Normalize(3);
+        Assert.AreEqual(new IcoLattice.PolarCoordinate(2, 0, 4, 2), c);
+      }
+
+      [Test]
+      public void NormalizeTropicSSW() {
+        var c = new IcoLattice.PolarCoordinate(1, 3, 4, 3);
+        c.Normalize(3);
+        Assert.AreEqual(new IcoLattice.PolarCoordinate(2, 0, 0, 0), c);
+      }
+
+      [Test]
+      public void Antipodes() {
+        var lattice = new IcoLattice(5);
+        foreach (var c in lattice.PolarCoordinates) {
+          Assert.AreEqual(0, (lattice.ToCartesian(c) + lattice.ToCartesian(-c)).sqrMagnitude, 1e-10);
+        }
+      }
+    }
+
+    [Test]
+    public void EnumerateEdges() {
+      var lattice = new Lattice<Vector3>(new IcoLattice(5));
+      var adjacency = new ISet<int>[lattice.Vertices.Count];
+      for (int i = 0; i < adjacency.Length; ++i) {
+        adjacency[i] = new HashSet<int>();
+      }
+
+      foreach (var edge in lattice.Edges) {
+        Assert.IsTrue(adjacency[edge.a].Add(edge.b), "Duplicate edge between vertices {0} and {1}.", edge.a, edge.b);
+        Assert.IsTrue(adjacency[edge.b].Add(edge.a), "Duplicate edge between vertices {0} and {1}.", edge.b, edge.a);
+      }
+
+      int fives = 0, sixes = 0;
+      for (int i = 0; i < adjacency.Length; ++i) {
+        var edges = adjacency[i];
+        int edgeCount = edges.Count;
+        if (edgeCount == 5) ++fives;
+        else if (edgeCount == 6) ++sixes;
+        else Assert.Fail("Edge count of vertex {0} should be 5 or 6; was {1}. Connected vertices: {2}", i, edgeCount, edges);
+      }
+      Assert.AreEqual(12, fives, "Expected exactly 12 vertices with 5 edges.");
+      Assert.AreEqual(adjacency.Length - 12, sixes, "Expected all other vertices with 6 edges.");
+    }
+
+    [Test]
+    public void VertexNormals() {
+      const int n = 5;
+      const float tolerance = .25f;
+
+      var lattice = new Lattice<Vector3>(new IcoLattice(n));
+      var delta = 2 * Mathf.PI / (5 * n);
+      var dsq = delta * delta;
+
+      foreach (var edge in lattice.Edges) {
+        Assert.AreEqual(dsq, (lattice.Vertices[edge.a] - lattice.Vertices[edge.b]).sqrMagnitude, (1 - (1 - tolerance) * (1 - tolerance))  * dsq);
+      }
+    }
+  }
+}
