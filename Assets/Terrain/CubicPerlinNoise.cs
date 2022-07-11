5acfc50b05466f1776db7ce6f46fa51cc2d251b2
Ross Wang <imagipioneer@gmail.com>
2022-07-11 13:01:04 -0700

    Cubic Perlin noise
    Since spherical Perlin noise using an icosahedral lattice takes ten dotproducts just to discriminate the face for a
    sampling vector (plus the three for the noise smapling, and two dots and two crosses for projection), the eight
    dotproducts for cubic Perlin noise is actually more efficient.
    
    While this isn't a performance critical area of code, the additional simplicity of cubic vs. spherical Perlin noise
    makes cubic a more effective choice.

---
+++ Assets/Terrain/CubicPerlinNoise.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Terrain
+{
+  public class CubicPerlinNoise
+  {
+    public readonly Vector3[,,] gradients;
+
+    private static IEnumerable<(int x, int y, int z)> IntCube(int resolution) {
+      for (int z = 0; z < resolution; ++z) {
+        for (int y = 0; y < resolution; ++y) {
+          for (int x = 0; x < resolution; ++x) {
+            yield return (x, y, z);
+          }
+        }
+      }
+    }
+
+    public CubicPerlinNoise(int resolution) {
+      gradients = new Vector3[resolution, resolution, resolution];
+      foreach (var (x, y, z) in IntCube(resolution)) { 
+        gradients[x, y, z] = Random.insideUnitSphere;
+      }
+    }
+
+    public float Sample(Vector3 v) {
+      Vector3Int i = Vector3Int.FloorToInt(v);
+      Vector3 rem = v - i;
+      var resolution = gradients.GetLength(0);
+      i -= resolution * Vector3Int.FloorToInt(v / resolution);
+
+      float sx = Mathf.SmoothStep(1, 0, rem.x),
+            sy = Mathf.SmoothStep(1, 0, rem.y),
+            sz = Mathf.SmoothStep(1, 0, rem.z);
+
+      float s = 0;
+      foreach (var (dx, dy, dz) in IntCube(2)) {
+        s += Vector3.Dot(gradients[(i.x + dx) % resolution,
+                                   (i.y + dy) % resolution,
+                                   (i.z + dz) % resolution], rem - new Vector3Int(dx, dy, dz))
+          * (dx == 0? sx : 1 - sx)
+          * (dy == 0? sy : 1 - sy)
+          * (dz == 0? sz : 1 - sz);
+      }
+
+      Debug.Assert(-1 <= s && s <= 1, string.Format("{0} is outside expected range [-1, 1]", s));
+
+      return s;
+    }
+  }
+}
--- Assets/Terrain/IcoDebug.cs
+++ Assets/Terrain/IcoDebug.cs
@@ -1,60 +1,64 @@
 ﻿using UnityEngine;
 using Assets.Lattice;
 
-public class IcoDebug : MonoBehaviour {
-  void Start() {
-    var lattice = new IcoLattice(5);
-    const int res = 512, ovr = 16;
-    var tex = new Texture2D(4 * res, 3 * res);
-    tex.wrapMode = TextureWrapMode.Clamp;
+namespace Assets.Terrain
+{
+  public class IcoDebug : MonoBehaviour
+  {
+    void Start() {
+      var noise = new CubicPerlinNoise(4);
+      const int res = 512, ovr = 16;
+      var tex = new Texture2D(4 * res, 3 * res);
+      tex.wrapMode = TextureWrapMode.Clamp;
 
-    Color Sample(float x, float y, float z) {
-      var v = new Vector3(x, y, z);
-      var rem = lattice.Project(v).rem;
-      return new Color(1 - rem.x - rem.y, rem.x, rem.y);
-    }
-
-    for (int y = 0; y < res; ++y) {
-      for (int x = 0; x < res; ++x) {
-        var v = 2 * new Vector2(x, y) / res - new Vector2(1, 1);
-        tex.SetPixel(res + x, y, Sample(-v.x, -1, v.y));
-        tex.SetPixel(res + x, 2 * res + y, Sample(-v.x, 1, -v.y));
-        tex.SetPixel(x, res + y, Sample(1, v.y, v.x));
-        tex.SetPixel(res + x, res + y, Sample(-v.x, v.y, 1));
-        tex.SetPixel(2 * res + x, res + y, Sample(-1, v.y, -v.x));
-        tex.SetPixel(3 * res + x, res + y, Sample(v.x, v.y, -1));
+      Color Sample(float x, float y, float z) {
+        var v = 2 * (new Vector3(x, y, z).normalized + Vector3.one);
+        var rem = noise.Sample(v);
+        return new Color(rem + 1, rem, 0);
       }
-    }
 
-    for (int y = 0; y < ovr; ++y) {
-      for (int x = 3 * res; x < 4 * res; ++x) {
-        tex.SetPixel(x, res - 1 - y, tex.GetPixel(x, res));
-        tex.SetPixel(x, 2 * res + y, tex.GetPixel(x, 2 * res - 1));
+      for (int y = 0; y < res; ++y) {
+        for (int x = 0; x < res; ++x) {
+          var v = 2 * new Vector2(x, y) / res - new Vector2(1, 1);
+          tex.SetPixel(res + x, y, Sample(-v.x, -1, v.y));
+          tex.SetPixel(res + x, 2 * res + y, Sample(-v.x, 1, -v.y));
+          tex.SetPixel(x, res + y, Sample(1, v.y, v.x));
+          tex.SetPixel(res + x, res + y, Sample(-v.x, v.y, 1));
+          tex.SetPixel(2 * res + x, res + y, Sample(-1, v.y, -v.x));
+          tex.SetPixel(3 * res + x, res + y, Sample(v.x, v.y, -1));
+        }
       }
-      for (int x = 0; x < res; ++x) {
-        {
-          tex.SetPixel(res - 1 - y, x, tex.GetPixel(res, x));
-          tex.SetPixel(2 * res + y, x, tex.GetPixel(2 * res - 1, x));
-        }
-        {
-          int ly = 3 * res - 1 - x;
-          tex.SetPixel(res - 1 - y, ly, tex.GetPixel(res, ly));
-          tex.SetPixel(2 * res + y, ly, tex.GetPixel(2 * res - 1, ly));
-        }
-        {
+
+      for (int y = 0; y < ovr; ++y) {
+        for (int x = 3 * res; x < 4 * res; ++x) {
           tex.SetPixel(x, res - 1 - y, tex.GetPixel(x, res));
           tex.SetPixel(x, 2 * res + y, tex.GetPixel(x, 2 * res - 1));
         }
-        {
-          int lx = 2 * res + x + ovr;
-          tex.SetPixel(lx, res - 1 - y, tex.GetPixel(lx, res));
-          tex.SetPixel(lx, 2 * res + y, tex.GetPixel(lx, 2 * res - 1));
+        for (int x = 0; x < res; ++x) {
+          {
+            tex.SetPixel(res - 1 - y, x, tex.GetPixel(res, x));
+            tex.SetPixel(2 * res + y, x, tex.GetPixel(2 * res - 1, x));
+          }
+          {
+            int ly = 3 * res - 1 - x;
+            tex.SetPixel(res - 1 - y, ly, tex.GetPixel(res, ly));
+            tex.SetPixel(2 * res + y, ly, tex.GetPixel(2 * res - 1, ly));
+          }
+          {
+            tex.SetPixel(x, res - 1 - y, tex.GetPixel(x, res));
+            tex.SetPixel(x, 2 * res + y, tex.GetPixel(x, 2 * res - 1));
+          }
+          {
+            int lx = 2 * res + x + ovr;
+            tex.SetPixel(lx, res - 1 - y, tex.GetPixel(lx, res));
+            tex.SetPixel(lx, 2 * res + y, tex.GetPixel(lx, 2 * res - 1));
+          }
         }
       }
+
+      tex.Apply();
+
+      GetComponent<Renderer>().material.mainTexture = tex;
     }
-
-    tex.Apply();
-
-    GetComponent<Renderer>().material.mainTexture = tex;
   }
 }
